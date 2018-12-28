8da756323a1deb42791d0e8c582ce4c94f956619
Aliana Britson <Britson.Aliana@deq.state.or.us>
2018-12-28 09:42:22 -0800

    not quite working....too many changes

--- app.R
+++ app.R
@@ -14,20 +14,26 @@ print("Initial data queries may take a few minutes.")
 library(shiny)
 library(AWQMSdata)
 
+#Need to remake query, cannot use AWQMS_Data as it pulls out too much data for the app to work,
+#plus, for NPDES only need a subset of data- 
+#the function NPDES_AWQMS_Qry will only pull water data since the year 2000 from a select set of monloc types
+
+#right now this is sourced, but will likely change it so that it is part of it's own library that gets pulled in at the beginning
+source("NPDES_AWQMSQuery.R")
 
 # Query out the valid values ---------------------------------------------
 
+#we could probably save a lot of time if I made a NPDES specific chars function 
+#or just did a list like for HUC names- NPDES characteristics don't really change
 chars <- AWQMS_Chars()
 chars <- chars$Char_Name
 chars <- sort(chars)
 
-station <- AWQMS_Stations()
+
+#changed to just the station types NPDES would be interested in (see function for details)
+station <- NPDES_AWQMS_Stations()
 station <- station$MLocID
 station <- sort(station)
-
-projects <- AWQMS_Projects()
-projects <- projects$Project
-projects <- sort(projects)
 
 organization <- AWQMS_Orgs()
 organization <- organization$OrganizationID
@@ -51,273 +57,243 @@ HUC8_Names <- c('Alsea', 'Alvord Lake', 'Applegate', 'Beaver-South Fork',
                 'Walla Walla', 'Wallowa', 'Warner Lakes', 'Williamson', 'Willow', 'Wilson-Trusk-Nestuccu', 'Yamhill')
 
 
+
 # Define UI 
 ui <- fluidPage(
-
-   # Application title
-   titlePanel("ORDEQ AWQMS data retrieval function for NPDES"),
-
-   # Sidebar with parameter inputs
-   sidebarLayout(
-      sidebarPanel(
-        # Start Date
-        dateInput("startd",
-                  label = "Select Start Date",
-                  min = '1949-09-15',
-                  value = '2010-01-01'
-                  ),
-
-        # End date
-        dateInput("endd",
-                  label = "Select End Date",
-                  min = '1900-1-1'),
-
-       #characteristics
-         selectizeInput("characteristics",
+  
+  # Application title
+  titlePanel("ORDEQ AWQMS data retrieval function for NPDES"),
+  
+  # Sidebar with parameter inputs
+  sidebarLayout(
+    sidebarPanel(
+      # Start Date
+      dateInput("startd",
+                label = "Select Start Date",
+                min = '1949-09-15',
+                value = '2000-01-01'),
+      
+      # End date
+      dateInput("endd",
+                label = "Select End Date",
+                min = '1900-1-1'),
+      
+      #characteristics
+      selectizeInput("characteristics",
                      "Select parameters",
                      choices = chars,
                      multiple = TRUE),
-
-       # Monitoring locations 
-        selectizeInput("monlocs",
-                        "Select Monitoring Locations",
-                        choices = station,
-                        multiple = TRUE),
-
-
-       # Projects 
-       selectizeInput("projs",
-                       "Select Projects",
-                       choices = projects,
-                       multiple = TRUE),
-
-       # huc8 names 
-       selectizeInput("huc8_nms",
-                       "Select HUC 8",
-                       choices = HUC8_Names,
-                       multiple = TRUE),
-        
-        
-       #Orgs
-       selectizeInput("orgs",
-                       "Select organization",
-                       choices = organization,
-                       multiple = TRUE)
-
-        ),
-
-
-     # Setup main panel
-       mainPanel(
-        h1("AWQMS_Data() Builder"),
-        h5("Select parameters on left to build data retrieval function"),
-        #tags$br(),
-        h5("Copy and paste function below into a different R session"),
-        h5("addition of output table and download button forthcoming- UNDER CONSTRUCTION"),
-        # Add line
-        tags$hr(),
-        #Add break
-        tags$br(),
-        textOutput("selected_chars"),
-        textOutput("sc1"),
-        textOutput("sc2"),
-        textOutput("sc3"),
-        textOutput("sc4"),
-        textOutput("sc5"),
-        # Aliana attempting to add table of data
-        tableOutput("table"),
-        #add a download button, see if I can't get it to work
-        downloadButton('downloadData', 'Download data')
-   )
-))
+      
+      # Monitoring locations 
+      selectizeInput("monlocs",
+                     "Select Monitoring Locations",
+                     choices = station,
+                     multiple = TRUE),
+      
+      # huc8 names 
+      selectizeInput("huc8_nms",
+                     "Select HUC 8",
+                     choices = HUC8_Names,
+                     multiple = TRUE),
+      
+      #Orgs
+      selectizeInput("orgs",
+                     "Select organization",
+                     choices = organization,
+                     multiple = TRUE),
+      
+      #Reject button
+      checkboxInput("Reject",
+                    label = "Keep Rejected data",
+                    value = FALSE)
+    ),
+    
+    
+    # Setup main panel
+    mainPanel(
+      h1("AWQMS Data Query"),
+      h5("Select parameters on left to build data retrieval function"),
+      #tags$br(),
+      h5("Copy and paste function below into a different R session"),
+      h5("addition of output table and download button forthcoming- UNDER CONSTRUCTION"),
+      # Add line
+      tags$hr(),
+      #Add break
+      tags$br(),
+      textOutput("selected_chars"),
+      textOutput("sc1"),
+      textOutput("sc2"),
+      textOutput("sc4"),
+      textOutput("sc5"),
+      # Aliana attempting to add table of data
+      tableOutput("table"),
+      #add a download button, see if I can't get it to work
+      downloadButton('downloadData', 'Download data')
+    )
+  ))
 
 # Define server logic required to display query
 server <- function(input, output) {
-
-#checks to see what exactly is getting put out  
-output$sc1<- renderText({if(length(input$monlocs) > 0)
+  
+  #checks to see what exactly is getting put out  
+  output$sc1<- renderText({if(length(input$monlocs) > 0)
   {toString(sprintf("'%s'", input$monlocs))} else {NULL}})
   
-output$sc2<- renderText({if(length(input$characteristics) > 0)  
-                  {toString(sprintf("'%s'", input$characteristics))} else {NULL}})
-
-output$sc3<- renderText({if(length(input$projs) > 0) 
-  {toString(sprintf("'%s'", input$projs))} else {NULL}})
-
-output$sc4<- renderText({if(length(input$huc8_nms) > 0)
+  output$sc2<- renderText({if(length(input$characteristics) > 0)  
+  {toString(sprintf("'%s'", input$characteristics))} else {NULL}})
+  
+  output$sc4<- renderText({if(length(input$huc8_nms) > 0)
   {toString(sprintf("'%s'", input$huc8_nms))} else {NULL}})
-
-output$sc5<- renderText({if(length(input$orgs) > 0) 
+  
+  output$sc5<- renderText({if(length(input$orgs) > 0) 
   {toString(sprintf("'%s'", input$orgs))} else {NULL}})
-
   
-   output$selected_chars <- renderText({
-
-     # Convert all field entries to strings of vectors - This allows their use in the query
-     stats <- toString(sprintf("'%s'", input$monlocs))
-     vals <- toString(sprintf("'%s'", input$characteristics))
-     proj_select <-toString(sprintf("'%s'", input$projs))
-     huc8s <-toString(sprintf("'%s'", input$huc8_nms))
-     organiz <- toString(sprintf("'%s'", input$orgs))
-
-     # Begin the query 
-     qry <- "AWQMS_Data("
-
   
-  # Add parameters to query - 
-     #General format - If field is not blank, and fields above are not blank add a comma and the paramter
-        # If above fields are all blank, do not add the comma
-        # If field is empty, do nothing
-     
-  #Start date
+  output$selected_chars <- renderText({
+    
+    # Convert all field entries to strings of vectors - This allows their use in the query
+    stats <- toString(sprintf("'%s'", input$monlocs))
+    vals <- toString(sprintf("'%s'", input$characteristics))
+    huc8s <-toString(sprintf("'%s'", input$huc8_nms))
+    organiz <- toString(sprintf("'%s'", input$orgs))
+    
+    # Begin the query 
+    qry <- "NPDES_AWQMS_Qry("
+    
+    # Add parameters to query - 
+    #General format - If field is not blank, and fields above are not blank add a comma and the paramter
+    # If above fields are all blank, do not add the comma
+    # If field is empty, do nothing
+    
+    #Start date
+    if(length(input$startd) > 0){
+      qry <- paste0(qry, "startdate = '", input$startd,"'" )
+    }
+    
+    #enddate
+    if(length(input$endd) > 0){
+      
       if(length(input$startd) > 0){
-       qry <- paste0(qry, "startdate = '", input$startd,"'" )
-     }
-
-  #enddate
-     if(length(input$endd) > 0){
-
-       if(length(input$startd) > 0){
-         qry <- paste0(qry, ", ")}
-
-       qry <- paste0(qry, "enddate = '", input$endd,"'" )
-     }
-
-  #monlocs
-       if(length(input$monlocs) > 0){
-
-       if(length(input$startd) > 0 |
-          length(input$endd) > 0){
-         qry <- paste0(qry, ", ")
-         }
-
-        qry <- paste0(qry,"station = c(",stats,")"  )
-
-
-       }
-
-  #chars
-     if(length(input$characteristics) > 0){
-
-
-       if(length(input$startd) > 0 |
-          length(input$endd) > 0|
-          length(input$monlocs) > 0){
-         qry <- paste0(qry, ", ")
-       }
-
-       qry <- paste0(qry,"char = c(",vals,") "  )
-
-     }
-
-  #projects
-     if(length(input$projs) > 0){
-
-       if(length(input$startd) > 0 |
-          length(input$endd) > 0|
-          length(input$monlocs) > 0|
-          length(input$characteristics) > 0){
-         qry <- paste0(qry, ", ")
-       }
-
-       qry <- paste0(qry,"project = c(",proj_select,") "  )
-
-     }
-
-  #sample media
-     #sample media
-     {
-       if(length(input$startd) > 0 |
-          length(input$endd) > 0|
-          length(input$monlocs) > 0|
-          length(input$characteristics) > 0|
-          length(input$stat_basis) > 0|
-          length(input$projs) > 0){
-         qry <- paste0(qry, ", ")
-       }
-       
-       qry <- paste0(qry,"media = c('Water') "  )  
-       
-     }
-       
-
-
- #HUC8s
-     if(length(input$huc8_nms) > 0){
-
-       if(length(input$startd) > 0 |
-          length(input$endd) > 0|
-          length(input$monlocs) > 0|
-          length(input$characteristics) > 0|
-          length(input$projs) > 0|
-          length(input$samp_med) > 0){
-         qry <- paste0(qry, ", ")
-       }
-
-       qry <- paste0(qry,"HUC8_Name = c(",huc8s,") "  )
-
-     }
-     
- #orgs
-     if(length(input$orgs) > 0){
-       
-       if(length(input$startd) > 0 |
-          length(input$endd) > 0|
-          length(input$monlocs) > 0|
-          length(input$characteristics) > 0|
-          length(input$projs) > 0|
-          length(input$samp_med) > 0|
-          length(input$huc8_nms) > 0){
-         qry <- paste0(qry, ", ")
-       }
-       
-       qry <- paste0(qry,"org = c(",organiz,") "  )
-       
-     }
-     
-
-     qry <- paste0(qry, ")")
-
-
-})
-   #table of queried data
-   #likely can't just put qry in expectin it to run, will likely have to make it more reactive than that
-   #qry is text, need to actually evaluate the statement and then render the Table
-   #need to use Travis' code above, but instead of outputting to a text query, have it evaluate 
-   #within the AWQMS_Data function
-   
-   #memory issue...can't allocate that much, which makes sense, need to find a way to get it to not 
-   #run query- or run it in SQL and not R......
-   #what if I put starting default values in that won't return anything or will return a small amount of data?
-   
-   #still having error: cannot coerce type 'closure' to vector of type 'character'
-   
-
-##make data reactive --definitely runs faster now
-   rstats<-reactive({if(length(input$monlocs) > 0) {toString(sprintf("'%s'", input$monlocs))} else {NULL}})
-   rvals<- reactive({if(length(input$characteristics) > 0) {toString(sprintf("'%s'", input$characteristics))} else {NULL}})
-   rproj_select<-reactive({if(length(input$projs) > 0) {toString(sprintf("'%s'", input$projs))} else {NULL}})
-   rhuc8s<-reactive({if(length(input$huc8_nms) > 0) {toString(sprintf("'%s'", input$huc8_nms))} else {NULL}})
-   rorganiz<-reactive({if(length(input$orgs) > 0) {toString(sprintf("'%s'", input$orgs))} else {NULL}})
-   rstdt<-reactive({input$startd})
-   rendd<-reactive({input$endd})
-   #try dat with just start and end date, see if any data is returned
-   #none yet, maybe it needs time? give it a few more minutes
-   #nope, it isn't returning anything.....
-   #we may need to scrap the function and go with a straight ODBC connection and pull from AWQMS....
-   dat<-reactive({AWQMS_Data(startdate=rstdt(),enddate=rendd(),media=c('Water'))})
-                             #,station=c(rstats()),project=c(rproj_select()),
-                   #char=c(rvals()),media=c('Water'),org=c(rorganiz()),HUC8=c(rhuc8s()),filterQC=TRUE)})
-   
-output$table<-renderTable({dat()})
-
-#only works in Chrome- and I have verified that it downloads a file with the headers and no data
-#the problem is with the query not pulling anything
-output$downloadData <- downloadHandler(
+        qry <- paste0(qry, ", ")}
+      
+      qry <- paste0(qry, "enddate = '", input$endd,"'" )
+    }
+    
+    #monlocs
+    if(length(input$monlocs) > 0){
+      
+      if(length(input$startd) > 0 |
+         length(input$endd) > 0){
+        qry <- paste0(qry, ", ")
+      }
+      
+      qry <- paste0(qry,"station = c(",stats,")"  )
+      
+    }
+    
+    #chars
+    if(length(input$characteristics) > 0){
+      
+      if(length(input$startd) > 0 |
+         length(input$endd) > 0|
+         length(input$monlocs) > 0){
+        qry <- paste0(qry, ", ")
+      }
+      
+      qry <- paste0(qry,"char = c(",vals,") "  )
+      
+    }
+    
+    #sample media
+    
+    qry <- paste0(qry,"media = c('Water') "  )  
+    
+    #HUC8s
+    if(length(input$huc8_nms) > 0){
+      
+      if(length(input$startd) > 0 |
+         length(input$endd) > 0|
+         length(input$monlocs) > 0|
+         length(input$characteristics) > 0){
+        qry <- paste0(qry, ", ")
+      }
+      
+      qry <- paste0(qry,"HUC8_Name = c(",huc8s,") "  )
+      
+    }
+    
+    #orgs
+    if(length(input$orgs) > 0){
+      
+      if(length(input$startd) > 0 |
+         length(input$endd) > 0|
+         length(input$monlocs) > 0|
+         length(input$characteristics) > 0|
+         length(input$huc8_nms) > 0){
+        qry <- paste0(qry, ", ")
+      }
+      
+      qry <- paste0(qry,"org = c(",organiz,") "  )
+      
+    }
+    
+    #reject filter
+    
+    if(!input$Reject) {
+      
+      if(length(input$startd) > 0 |
+         length(input$endd) > 0|
+         length(input$monlocs) > 0|
+         length(input$characteristics) > 0|
+         length(input$huc8_nms) > 0|
+         length(input$orgs) > 0){
+        qry <- paste0(qry, ", ")
+      }
+      
+      qry <- paste0(qry,"reject = TRUE")  
+      
+    }
+    
+    qry <- paste0(qry, ")")
+    
+  })
+  #table of queried data
+  #likely can't just put qry in expectin it to run, will likely have to make it more reactive than that
+  #qry is text, need to actually evaluate the statement and then render the Table
+  #need to use Travis' code above, but instead of outputting to a text query, have it evaluate 
+  #within the AWQMS_Data function
   
-  filename = function() {paste("dataset-", Sys.Date(), ".csv", sep="")},
-  content = function(file) {write.csv(dat(), file,row.names = FALSE)})
-
+  #memory issue...can't allocate that much, which makes sense, need to find a way to get it to not 
+  #run query- or run it in SQL and not R......
+  #what if I put starting default values in that won't return anything or will return a small amount of data?
+  
+  #still having error: cannot coerce type 'closure' to vector of type 'character'
+  
+  
+  ##make data reactive --definitely runs faster now
+  rstats<-reactive({if(length(input$monlocs) > 0) {toString(sprintf("'%s'", input$monlocs))} else {NULL}})
+  rvals<- reactive({if(length(input$characteristics) > 0) {toString(sprintf("'%s'", input$characteristics))} else {NULL}})
+  rhuc8s<-reactive({if(length(input$huc8_nms) > 0) {toString(sprintf("'%s'", input$huc8_nms))} else {NULL}})
+  rorganiz<-reactive({if(length(input$orgs) > 0) {toString(sprintf("'%s'", input$orgs))} else {NULL}})
+  rstdt<-reactive({input$startd})
+  rendd<-reactive({input$endd})
+  rrej<-reactive({if(!input$Reject) {TRUE} else {FALSE} })
+  #try dat with just start and end date, see if any data is returned
+  #none yet, maybe it needs time? give it a few more minutes
+  #nope, it isn't returning anything.....
+  #we may need to scrap the function and go with a straight ODBC connection and pull from AWQMS....
+  dat<-reactive({NPDES_AWQMS_Qry(startdate=rstdt(),enddate=rendd(),station=c(rstats()),
+                                 char=c(rvals()),org=c(rorganiz()),HUC8_Name=c(rhuc8s()),reject=rrej())})
+  
+  output$table<-renderTable({dat()})
+  
+  #only works in Chrome- and I have verified that it downloads a file with the headers and no data
+  #the problem is with the query not pulling anything
+  output$downloadData <- downloadHandler(
+    
+    filename = function() {paste("dataset-", Sys.Date(), ".csv", sep="")},
+    content = function(file) {write.csv(dat(), file,row.names = FALSE)})
+  
 }
 # Run the application
 shinyApp(ui = ui, server = server)
