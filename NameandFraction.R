06096766f01077a627db2893ee3006c5bd996b72
Aliana Britson <Britson.Aliana@deq.state.or.us>
2019-03-11 12:56:21 -0700

    Created function to combine sample fraction and metal parameter names (was in code, but needed it in more than one place).

---
+++ NameandFraction.R
@@ -0,0 +1,11 @@
+#function to add sample fraction to metals for NPDES RPA purposes
+
+namefrac<-function(x)
+{x$Char_Name<-
+  ifelse(x$Char_Name %in% c("Calcium","Copper","Magnesium","Potassium","Sodium","Cyanide","Aluminum","Iron","Lead",
+                                  "Mercury","Nickel","Silver","Thallium","Antimony","Arsenic","Beryllium","Cadmium","Chromium",
+                                  "Zinc","Selenium","Chromium(III)","Chromium(VI)","Arsenic ion (3+)","Methylmercury(1+)"),
+         paste0(x$Char_Name,", ",x$Sample_Fraction),
+         x$Char_Name)
+  return(x)
+}
