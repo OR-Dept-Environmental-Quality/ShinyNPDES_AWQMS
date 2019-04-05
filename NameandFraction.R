6b5770c6d374a4c8931ae467f15c7b1bcbf028ef
Aliana Britson <Britson.Aliana@deq.state.or.us>
2019-04-05 14:12:51 -0700

    added Organic Carbon to namefrac function

--- NameandFraction.R
+++ NameandFraction.R
@@ -4,7 +4,7 @@ namefrac<-function(x)
 {x$Char_Name<-
   ifelse(x$Char_Name %in% c("Calcium","Copper","Magnesium","Potassium","Sodium","Cyanide","Aluminum","Iron","Lead",
                                   "Mercury","Nickel","Silver","Thallium","Antimony","Arsenic","Beryllium","Cadmium","Chromium",
-                                  "Zinc","Selenium","Chromium(III)","Chromium(VI)","Arsenic ion (3+)","Methylmercury(1+)"),
+                                  "Zinc","Selenium","Chromium(III)","Chromium(VI)","Arsenic ion (3+)","Methylmercury(1+)","Organic carbon"),
          paste0(x$Char_Name,", ",x$Sample_Fraction),
          x$Char_Name)
   return(x)
