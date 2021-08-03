17055658246411142605cd96828bd9b2966db8b8
Aliana Britson <Britson.Aliana@deq.state.or.us>
2021-08-03 09:59:55 -0700

    added uS/cm

--- CalcHardness_Function.R
+++ CalcHardness_Function.R
@@ -20,7 +20,8 @@ hardness<-function(x){
   dm<-subset(x,x$Sample_Fraction %in% "Dissolved" & x$Char_Name %in% "Magnesium")
   
   #select all specific conductance data, just get data in the right units for now - I don't have any translators
-  cond<-subset(x,x$Char_Name %in% c("Conductance","Specific conductance","Specific conductivity","Conductivity") & x$Result_Unit %in% "umho/cm")
+  #note that umho/cm and uS/cm don't need any conversion.
+  cond<-subset(x,x$Char_Name %in% c("Conductance","Specific conductance","Specific conductivity","Conductivity") & x$Result_Unit %in% c("umho/cm","uS/cm"))
   
   #add them all together to the new dataset
   new$total.Calcium<-tc$Result_Numeric[match(new$act_id,tc$act_id,nomatch=NA)]
@@ -29,7 +30,7 @@ hardness<-function(x){
   new$diss.Magnesium<-dm$Result_Numeric[match(new$act_id,dm$act_id,nomatch=NA)]
   new$Conduct<-cond$Result_Numeric[match(new$act_id,cond$act_id,nomatch=NA)]
   
-  #remove activity id's with no Ca, Mg, or conductuance data that made it through
+  #remove activity id's with no Ca, Mg, or conductance data that made it through
   new<-subset(new,!(is.na(new$total.Calcium) & is.na(new$diss.Calcium) & is.na(new$total.Magnesium) & is.na(new$diss.Magnesium) & is.na(new$Conduct)))
   
   #calculate hardness. If total Ca and Mg are there, use those, if not then use dissolved Ca and Mg, if no Ca or Mg data, use conductance
@@ -53,7 +54,7 @@ hardness<-function(x){
 
 
 
-library(AWQMSdata)
+#library(AWQMSdata)
 #x<-AWQMS_Data(startdate='01-01-2010',char=c("Calcium","Magnesium","Conductance","Specific conductance","Specific conductivity","Conductivity"))
 
 #try<-hardness(x)
